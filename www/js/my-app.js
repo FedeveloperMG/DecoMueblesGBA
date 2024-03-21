0c06d710fa1c9dcba653176073e61bf483cd9998
Federico <federicomartingodoy@gmail.com>
2024-03-20 22:41:51 -0300

    Agregando boton detalles - Terminando ordenamiento y filtros - Agregando pagina insumos

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,61 +2,86 @@
 var $$ = Dom7;
 
 var app = new Framework7({
-    // App root element
-    root: '#app',
-    // App Name
-    name: 'DecoMuebles',
-    // App id
-    id: 'com.myapp.test',
-    // Enable swipe panel
-    panel: {
-      swipe: 'left',
-    },
-    // Add default routes
-    routes: [
-      { path: '/index/', url: 'index.html', options: { transition: 'f7-cover' } },
-      { path: '/products/', url: 'products.html', options: { transition: 'f7-cover' } },
-      { path: '/vendors/', url: 'vendors.html', options: { transition: 'f7-cover' } },
-      { path: '/prices/', url: 'prices.html', options: { transition: 'f7-cover' } },
-      { path: '/fliers/', url: 'fliers.html', options: { transition: 'f7-cover' } },
-      { path: '/newProduct/', url: 'newProduct.html', options: { transition: 'f7-cover' } },
-    ]
-    // ... other parameters
-  });
+  // App root element
+  root: '#app',
+  // App Name
+  name: 'DecoMuebles',
+  // App id
+  id: 'com.myapp.test',
+  // Enable swipe panel
+  panel: {
+    swipe: 'left',
+  },
+  // Add default routes
+  routes: [
+    { path: '/index/', url: 'index.html', options: { transition: 'f7-cover' } },
+    { path: '/products/', url: 'products.html', options: { transition: 'f7-cover' } },
+    { path: '/vendors/', url: 'vendors.html', options: { transition: 'f7-cover' } },
+    { path: '/prices/', url: 'prices.html', options: { transition: 'f7-cover' } },
+    { path: '/fliers/', url: 'fliers.html', options: { transition: 'f7-cover' } },
+    { path: '/newProduct/', url: 'newProduct.html', options: { transition: 'f7-cover' } },
+    { path: '/detailsProduct/', url: 'detailsProduct.html', options: { transition: 'f7-cover' } },
+    { path: '/insumos/', url: 'insumos.html', options: { transition: 'f7-cover' } },
+    { path: '/manoObra/', url: 'manoObra.html', options: { transition: 'f7-cover' } },
+  ]
+  // ... other parameters
+});
 
 var mainView = app.views.create('.view-main');
 
 // Handle Cordova Device Ready Event
-$$(document).on('deviceready', function() {
-    console.log("Device is ready!");
+$$(document).on('deviceready', function () {
+  console.log("Device is ready!");
 });
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
-    
+
 })
 
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
   $$("#toListProducts").on('click', toListProducts)
 })
 
-// Option 2. Using live 'page:init' event handlers for each page
+
 $$(document).on('page:init', '.page[data-name="products"]', function (e) {
   $$("#btnListProduct").on('click', verProductos)
-  $$("#filtroProveedor").on("change", filtrarProv)
-  $$("#filtroNombre").on("change", filtrarNombre)
+  $$("#filtroNombre").on("input", filtrarNombre)
+  $$("#ordenBy").on("change", orderBy)
 })
 $$(document).on('page:init', '.page[data-name="newProduct"]', function (e) {
   $$("#btnAddProducto").on('click', añadirProducto)
 })
-// Option 2. Using live 'page:init' event handlers for each page
+
 $$(document).on('page:init', '.page[data-name="vendors"]', function (e) {
 })
-// Option 2. Using live 'page:init' event handlers for each page
+
 $$(document).on('page:init', '.page[data-name="prices"]', function (e) {
 })
-// Option 2. Using live 'page:init' event handlers for each page
+
 $$(document).on('page:init', '.page[data-name="fliers"]', function (e) {
+})
+
+$$(document).on('page:init', '.page[data-name="detailsProduct"]', function (e) {
+  $$("#detailsProductTitle").text(tituloProductoDetallado);
+})
+$$(document).on('page:init', '.page[data-name="insumos"]', function (e) {
+  $$("#productTitle").text(tituloInsumos);
+
+  $$("#cantLijas").text(cantLijas);
+  $$("#cantPinceles").text(cantPinceles)
+  $$("#cantRodillos").text(cantRodillos);
+  $$("#cantPintura").text(cantPintura);
+  $$("#cantCola").text(cantCola)
+  $$("#cantSilastic").text(cantSilastic);
+  $$("#cantVidrio").text(cantVidrio);
+  $$("#cantDiluyente").text(cantDiluyente);
+  $$("#cantMueble").text(cantMueble);
+  $$("#cantFlete").text(cantFlete)
+  $$("#cantLuz").text(cantLuz);
+
+})
+$$(document).on('page:init', '.page[data-name="manoObra"]', function (e) {
 })
 
 
@@ -70,10 +95,17 @@ colManoDeObra = db.collection('MANODEOBRA')
 
 /* --------------------------- Variables globales --------------------------- */
 var products = []
-
-
+var tituloProductoDetallado, tituloInsumos, detalleLija, detallePincel, detalleRodillo, detallePintura, detalleCola, detalleSilastic, detalleVidrio, detalleDiluyente, detalleMueble, detalleFlete, detalleLuz, detalleHsProducto
+var cantLijas, cantPinceles, cantRodillos, cantPintura, cantCola, cantSilastic, cantVidrio, cantDiluyente, cantMueble, cantFlete, cantLuz
+var variableOrdenamiento = "Proveedor"
+var valorOrdenamiento = "asc"
+var variableQuery = "Proveedor"
+var operadorQuery = "!="
+var valorQuery = "-"
 
 /* -------------------------------- Funciones ------------------------------- */
+
+//Función para añadir productos a db
 function añadirProducto() {
 
   //Inputs
@@ -98,7 +130,7 @@ function añadirProducto() {
   mueble = $$("#newPMueble").val()
   proveedor = $$("#newPProveedor").val()
 
-  producto = 
+  producto =
   {
     Nombre: nombre,
     Tipo: tipo,
@@ -119,158 +151,159 @@ function añadirProducto() {
     Diluyente: diluyente,
     Flete: flete,
     Mueble: mueble,
-    Costo: 0 ,
+    Costo: 0,
     PrecioFinal: 0,
     Proveedor: proveedor
   }
 
-  if(proveedor == "-"){
+  if (proveedor == "-") {
     app.dialog.alert("Ingrese el Proveedor!")
-  }else{ 
+  } else {
     colProductos.add(producto)
-    .then(function(doc){
-      console.log("Producto agregado");
-      
-    })
-    .catch(function(err){
-      console.log("Error al agregar producto")
-    })
+      .then(function (doc) {
+        console.log("Producto agregado");
+
+      })
+      .catch(function (err) {
+        console.log("Error al agregar producto")
+      })
   }
 }
 
 
+//Función para ver lista de productos
 function verProductos() {
 
-    colProductos.get()
-    .then(function(res){
-      res.forEach(function (doc){
+  
+  colProductos.orderBy(variableOrdenamiento, valorOrdenamiento).where(variableQuery, operadorQuery, valorQuery).get()
+    .then(function (res) {
+      res.forEach(function (doc) {
         info = doc.data()
-        
-        console.log(info);
-
         products.push(info);
-
-        console.log(products);
       });
 
-        // Iterar sobre el array
-        products.forEach(function(valor) {
-          // Crear nueva fila
-          var nuevaFila = $$('<tr>');
-          
-          // Crear celda para el valor
-          var celdaNombre = $$('<td>').text(`${valor.Nombre}-${valor.Tipo}`);
-          var celdaColor = $$('<td>').text(valor.Color);
-          var celdaPrecio = $$('<td>').text(valor.PrecioFinal);
-          var celdaMedida = $$('<td>').text(`${valor.Ancho}x${valor.Largo}x${valor.Ancho}`);
-          var celdaStock = $$('<td>').text(valor.Stock);
-          var celdaProveedor = $$('<td>').text(valor.Proveedor);
-          
-          // Agregar celdas a la fila
-          nuevaFila.append(celdaNombre, celdaColor, celdaPrecio, celdaMedida, celdaStock, celdaProveedor);
-          
-          // Agregar fila al cuerpo de la tabla
-          $$("#tableProducts").append(nuevaFila);
+      // Iterar sobre el array
+      for (i = 0; i < products.length; i++) {
+        
+        // Crear nueva fila
+        var nuevaFila = $$('<tr>');
 
-          $$("#btnListProduct").removeClass("btnVisible").addClass("btnOculto")     
-          $$("#cajaFiltroProv").removeClass("btnOculto").addClass("btnVisible")     
-          $$("#cajaFiltroNombre").removeClass("btnOculto").addClass("btnVisible")     
-        });
-  })
-    .catch(function(err){
+        // Crear celda para el valor
+        var celdaNombre = $$('<td>').text(`${products[i].Nombre}-${products[i].Tipo}`);
+        var celdaColor = $$('<td>').text(products[i].Color);
+        var celdaPrecio = $$('<td>').text(products[i].PrecioFinal);
+        var celdaMedida = $$('<td>').text(`${products[i].Ancho}x${products[i].Largo}x${products[i].Ancho}`);
+        var celdaStock = $$('<td>').text(products[i].Stock);
+        var celdaProveedor = $$('<td>').text(products[i].Proveedor);
+        
+          btnDetalles = $$(`<input id="${i}" type="button" value="+" class="button button-small button-round button-fill color-green">`)
+          btnDetalles.data("valorId", `${i}`)
+          btnDetalles.on("click", function () {
+            var dataId = $$(this).data("valorId")
+
+            tituloProductoDetallado = `${products[dataId].Nombre}-${products[dataId].Tipo}`
+            tituloInsumos = `${products[dataId].Nombre}-${products[dataId].Tipo}`
+
+            // Crear celda para el valor
+           cantLijas = `${products[dataId].Lijas}`
+           cantPinceles = `${products[dataId].Pinceles}`
+           cantRodillos = `${products[dataId].Rodillos}`
+           cantPintura = `${products[dataId].Pintura}`
+           cantCola = `${products[dataId].Cola}`
+           cantSilastic = `${products[dataId].Silastic}`
+           cantVidrio = `${products[dataId].Vidrios}`
+           cantDiluyente = `${products[dataId].Diluyente}`
+           cantMueble = `${products[dataId].Mueble}`
+           cantFlete = `${products[dataId].Flete}`
+           cantLuz = `${products[dataId].Luz}`
+           
+           console.log(products);
+           
+           console.log(cantLijas, cantPinceles, cantRodillos, cantPintura, cantCola, cantSilastic, cantVidrio, cantDiluyente, cantMueble, cantFlete, cantLuz);
+           
+           // Agregar celdas a la fila
+           mainView.router.navigate("/detailsProduct/")
+
+          })
+
+        
+        var celdaDetalles = $$('<td>').append(btnDetalles);
+
+        // Agregar celdas a la fila
+        nuevaFila.append(celdaNombre, celdaColor, celdaPrecio, celdaMedida, celdaStock, celdaProveedor, celdaDetalles);
+
+        // Agregar fila al cuerpo de la tabla
+        $$("#tableProducts").append(nuevaFila);
+
+      }
+
+
+      $$("#btnListProduct").removeClass("btnVisible").addClass("btnOculto")
+      $$("#cajaFiltroNombre").removeClass("btnOculto").addClass("btnVisible")
+      $$("#cajaOrdenar").removeClass("btnOculto").addClass("btnVisible")
+    })
+    .catch(function (err) {
       console.log("Error al listar productos")
-  })  
+    })
 }
 
-function toListProducts (){
+
+function toListProducts() {
   products = []
 }
 
-function filtrarProv (){
-
-  products = []
-  $$("#tableProducts").html("")
-
-
-  proveedorElegido = $$("#filtroProveedor").val()
-
-  colProductos.where("Proveedor", "==", proveedorElegido).get()
-  .then(function(res){
-    res.forEach(function (doc){
-      info = doc.data()
-      
-      console.log(info);
-
-      products.push(info);
-
-      console.log(products);
-    });
-
-     // Iterar sobre el array
-     products.forEach(function(valor) {
-      // Crear nueva fila
-      var nuevaFila = $$('<tr>');
-      
-      // Crear celda para el valor
-      var celdaNombre = $$('<td>').text(`${valor.Nombre}-${valor.Tipo}`);
-      var celdaColor = $$('<td>').text(valor.Color);
-      var celdaPrecio = $$('<td>').text(valor.PrecioFinal);
-      var celdaMedida = $$('<td>').text(`${valor.Ancho}x${valor.Largo}x${valor.Ancho}`);
-      var celdaStock = $$('<td>').text(valor.Stock);
-      var celdaProveedor = $$('<td>').text(valor.Proveedor);
-      
-      // Agregar celdas a la fila
-      nuevaFila.append(celdaNombre, celdaColor, celdaPrecio, celdaMedida, celdaStock, celdaProveedor);
-      
-      // Agregar fila al cuerpo de la tabla
-      $$("#tableProducts").append(nuevaFila);
-      products = []
-  })
-
-}
-)}
-
-
-function filtrarNombre (){
-
+//Función para filtrar lista por nombre
+function filtrarNombre() {
   products = []
   $$("#tableProducts").html("")
+  nombreElegido = $$("#filtroNombre").val().toLowerCase()
 
+  nombreModificado = nombreElegido.charAt(0).toUpperCase() + nombreElegido.slice(1).toLowerCase()
 
-  nombreElegido = $$("#filtroNombre").val()
+  if (nombreModificado.length !== 0) {
+    variableOrdenamiento = "Nombre"
+    valorOrdenamiento = "asc"
+    variableQuery = "Nombre"
+    valorQuery = nombreModificado
+    operadorQuery = "=="
+    verProductos()
+  }else if(nombreModificado.length == 0){
+    variableOrdenamiento = "Proveedor"
+    valorOrdenamiento = "asc"
+    variableQuery = "Proveedor"
+    operadorQuery = "!="
+    valorQuery = "-"
+    verProductos()
+  }
+}
 
-  colProductos.where("Nombre", "==", nombreElegido).get()
-  .then(function(res){
-    res.forEach(function (doc){
-      info = doc.data()
-      
-      console.log(info);
+//Función para ordenar lista
+function orderBy(){
+  
+  $$("#tableProducts").html("")
+  products = []
+  orderValue = $$("#ordenBy").val()
 
-      products.push(info);
+  if (orderValue == "PrecioMenMay") {
+    variableOrdenamiento = "PrecioFinal"
+    valorOrdenamiento = "asc"
+    variableQuery = "PrecioFinal"
+    operadorQuery = "!="
+    valorQuery = "-"
+  }else if (orderValue == "PrecioMayMen") {
+    variableOrdenamiento = "PrecioFinal"
+    valorOrdenamiento = "desc"
+    variableQuery = "PrecioFinal"
+    operadorQuery = "!="
+    valorQuery = "-"
+  }else if (orderValue == "Proveedor"){
+    variableOrdenamiento = "Proveedor"
+    valorOrdenamiento = "asc"
+    variableQuery = "Proveedor"
+    operadorQuery = "!="
+    valorQuery = "-"
+  }
 
-      console.log(products);
-    });
-
-     // Iterar sobre el array
-     products.forEach(function(valor) {
-      // Crear nueva fila
-      var nuevaFila = $$('<tr>');
-      
-      // Crear celda para el valor
-      var celdaNombre = $$('<td>').text(`${valor.Nombre}-${valor.Tipo}`);
-      var celdaColor = $$('<td>').text(valor.Color);
-      var celdaPrecio = $$('<td>').text(valor.PrecioFinal);
-      var celdaMedida = $$('<td>').text(`${valor.Ancho}x${valor.Largo}x${valor.Ancho}`);
-      var celdaStock = $$('<td>').text(valor.Stock);
-      var celdaProveedor = $$('<td>').text(valor.Proveedor);
-      
-      // Agregar celdas a la fila
-      nuevaFila.append(celdaNombre, celdaColor, celdaPrecio, celdaMedida, celdaStock, celdaProveedor);
-      
-      // Agregar fila al cuerpo de la tabla
-      $$("#tableProducts").append(nuevaFila);
-      products = []
-  })
+  verProductos()
 
 }
-)}
